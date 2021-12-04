638f1020813a8f26f9a8145521ab2bbbd88a184f
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-04 11:44:55 +0000

    Show error if present in run script

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 (defn exec [part]
   (let [
       day    (first *command-line-args*)
-      folder (str "./" day)]
-    (:out 
-      (shell/sh 
-        "bash" "-c" (str "cat " folder "/input.txt | " folder "/" part ".sh")))))
+      folder (str "./" day)
+      {out :out, err :err}   (select-keys 
+               (shell/sh "bash" "-c" (str "cat " folder "/input.txt | " folder "/" part ".sh")) 
+               [:out :err])]
+      (str (str/trim out) (str/trim err))
+   ))
 
-(print "Part 1: " (exec "part1"))
-(print "Part 2: " (exec "part2"))
+(println "Part 1: " (exec "part1"))
+(println "Part 2: " (exec "part2"))
