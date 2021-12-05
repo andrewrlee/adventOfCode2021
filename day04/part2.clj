b86ad05f7fa6316e2bb42174ad0afbad32340c41
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-05 17:29:52 +0000

    Day 4 part 2

---
+++ day04/part2.clj
@@ -0,0 +1,47 @@
+#!/usr/bin/env bb -i
+
+(def input *input*)
+
+(defn transpose [matrix] (apply map list matrix))
+
+(defn read-numbers [s re] (map #(Integer/parseInt %) (filter (complement empty?) (str/split s re))) )
+
+(def numbers (read-numbers (first input) #","))
+
+(defn read-grid [string-rows] 
+  (->> string-rows
+       (map #(read-numbers % #"\s+"))))
+
+(def grids 
+  (->> input
+     (rest)
+     (partition-by str/blank?)
+     (filter #(not= (count %) 1))
+     (map read-grid)))
+
+(defn find-match [numbers strips] 
+  (letfn [(is-strip-complete? [numbers strip] (every? #(contains? (set numbers) %) strip))]
+    (some #(is-strip-complete? numbers %) strips)))
+
+(defn is-bingo? [numbers grid] 
+  (if (or (find-match numbers grid) (find-match numbers (transpose grid))) :winners :still-in-play))
+
+(defn unmarked-numbers [grid called-numbers]
+   (clojure.set/difference (set (flatten grid)) (set called-numbers)))
+
+(defn bingo-score [grid called-numbers this-number] 
+  (* this-number (apply + (unmarked-numbers grid called-numbers))))
+  
+(defn play
+  ([] (play grids [] numbers []))
+  ([remaining-grids called [this-number & remaining-numbers] results]
+   (cond
+     (empty? remaining-grids) results
+     :else 
+     (let [called-numbers                   (conj called this-number)
+           {:keys [winners still-in-play]}  (group-by #(is-bingo? called-numbers %) remaining-grids)
+           these-winners                    (map #(bingo-score % called-numbers this-number) winners)]
+             (play still-in-play called-numbers remaining-numbers (concat results these-winners))))))
+
+(last (play))
+
