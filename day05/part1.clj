1954546ce42de7007883129ff40f663469670357
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-06 10:19:46 +0000

    initial commit of day 05 part 1

---
+++ day05/part1.clj
@@ -0,0 +1,22 @@
+#!/usr/bin/env bb -i
+
+(defn read-points [s] (->>
+   s
+   ( #(str/split % #" -> "))
+   (map #(str/split % #","))
+   (map (fn[[x y]] [(Integer/parseInt x) (Integer/parseInt y)]))))
+
+(defn points-between [start, end]
+   (let [
+     xinc (compare (first end) (first start))
+     yinc (compare (second end) (second start))]
+   (->>
+     (iterate (fn [[x y]] [(+ xinc x) (+ y yinc)]) start)
+     (take-while #(not= % end))
+     ( #(concat % [end]) ))))
+
+(->> *input*
+  (map read-points)     
+     
+)
+
