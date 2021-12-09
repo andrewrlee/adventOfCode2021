e51d47924cfc2b3954c9d5433220ca77c319dccc
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-09 00:19:35 +0000

    tidy up

--- day08/part2.clj
+++ day08/part2.clj
@@ -1,9 +1,8 @@
 #!/usr/bin/env bb -i
 
 (defn has-length [n] (fn [s] (= (count s) n)))
-
 (defn contains         [col]   (fn [s] (= (count col) (count (clojure.set/intersection (set s) col)))))
-(defn does-not-contain [col]   (fn [s] (not= (count col) (count (clojure.set/intersection (set s) col)))))
+(defn does-not-contain [col]   (fn [s] (not ((contains col) s))))
 (defn has-in-common    [n col] (fn [s] (= n (count (clojure.set/intersection col (set s))))))
 
 (defn find-with [language pred] (first (filter pred language)))
@@ -17,27 +16,25 @@
         
         six-char-length (filter (has-length 6) language)
         six   (find-with six-char-length (does-not-contain one))
-        zero  (find-with six-char-length (fn [s] (and ((contains one) s) ((does-not-contain four) s))))
+        zero  (find-with six-char-length #(and ((contains one) %) ((does-not-contain four) %)))
         nine  (find-with six-char-length (contains four))
 
         five-char-length (filter (has-length 5) language)
         three (find-with five-char-length (contains one))
-        two   (find-with five-char-length (fn [s] (and (not ((contains one)s)) ((has-in-common 4 six) s))))
-        five  (find-with five-char-length (has-in-common 5 six))
-        ]
+        two   (find-with five-char-length #(and ((does-not-contain one) %) ((has-in-common 4 six) %)))
+        five  (find-with five-char-length (has-in-common 5 six))]
     { zero 0 one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9}))
 
 (defn decode [[language input]]
   (let [codex (determine-numbers (map set language))
         code  (map set input)]  
-    (apply str (map #(codex %) code))))
+    (Integer/parseInt (apply str (map #(codex %) code)))))
 
 (defn process-line [line]
   (->> 
-     line 
+     line
      ( #(str/split % #" \| ") )
      (map #(str/split % #"\s+"))
-     (decode)
-     (#(Integer/parseInt %))))
+     (decode)))
 
-(->> *input*(map process-line) (reduce +))
+(->> *input* (map process-line) (reduce +))
