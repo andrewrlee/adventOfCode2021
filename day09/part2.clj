dde5dc337731081aee188c1eaaec9af5f78118d4
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-11 09:05:11 +0000

    almost working

--- day09/part2.clj
+++ day09/part2.clj
@@ -9,25 +9,25 @@
 (defn get-surrounding-points [grid [x y]] 
   (let [deltas             [[0 1] [1 0] [0 -1] [-1 0]]
         surrounding-points (map (fn [[x2 y2]] [(+ x x2) (+ y y2)]) deltas)] 
-    (remove (fn [[point value]] (or (nil? value) (= value 9))) (map #(identity [% (grid %)]) surrounding-points)))) 
+    (map first (remove (fn [[point value]] (or (nil? value) (= value 9))) (map #(identity [% (grid %)]) surrounding-points))))) 
 
-(defn find-basin [grid seen [point value]]
+(defn find-basin [grid seen point]
   (let [surrounding-points  (get-surrounding-points grid point)
-        to-process          (filter (fn [[point]] (not (contains? seen point))) surrounding-points)
-        new-seen            (clojure.set/union #{point} seen (set (map first surrounding-points)))]
-
-    (println point to-process "<<")
+        to-process          ( filter (fn [point] (not (contains? seen point))) surrounding-points)
+        new-seen            (clojure.set/union #{point} seen (set surrounding-points))
+        to-add              (clojure.set/difference (clojure.set/union #{point} (set to-process)) seen)]
     (cond 
-      (empty? to-process) [[point] [[new-seen]]]
-      :else (println (mapcat #(find-basin grid new-seen %) to-process)))))
+      (empty? to-process) to-add
+      :else (let [result (mapcat #(find-basin grid new-seen %) to-process)]
+             (set (concat result (set to-add)))))))
 
 (defn add-to-basin [grid]
   (fn [{:keys [seen basins]} point]
     (if (contains? seen point) 
-      {:seen #{} :basins [] }
-      (let [[new-basin new-seen] (find-basin grid seen point)]
-        (println point "###########" basins  " ! " seen )
-        {:seen #{} :basins [] }
+      {:seen seen }
+      (let [basin (find-basin grid seen point)]
+        (println point "###########"  " ! " basin )
+        {:seen (clojure.set/union seen basin) :basins (conj basins basin ) }
       ))))
 
 (->> *input*
@@ -36,4 +36,6 @@
      (#(let [grid (into (sorted-map) %)]
          (->> grid
               (filter (fn [[_ value]] (not= 9 value)))
-              (reduce (add-to-basin grid) {:seen #{} :basins [] })))))
+              (map first)
+              (reduce (add-to-basin grid) {:seen #{} :basins []})
+              (:basins)))))
