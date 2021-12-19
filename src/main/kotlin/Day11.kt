205fc000fd02fdbe2168a78d503bf44dfa41cd62
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2021-12-19 18:28:01 +0000

    day 11 and 12

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -2,36 +2,87 @@ import java.io.File
 import java.nio.charset.Charset.defaultCharset
 import kotlin.streams.toList
 
-object Day11 {
-    object Part1 {
-        fun toCoordinates(row: Int, line: String) = line.chars()
-            .map(Character::getNumericValue)
-            .toList()
-            .mapIndexed { col, value -> (row to col) to value }
+typealias OctopusGrid = Map<Pair<Int, Int>, Day11.Octopus>
 
-        fun getSurroundingCoordinates(
-            validCells: ValidCells,
-            coord: Coordinate,
+object Day11 {
+    private fun toCoordinates(row: Int, line: String) = line.chars()
+        .map(Character::getNumericValue)
+        .toList()
+        .mapIndexed { col, value -> (row to col) to Octopus((row to col), value) }
+
+    val octopusGrid = {
+        File("day11/input-real.txt")
+            .readLines(defaultCharset())
+            .mapIndexed(::toCoordinates)
+            .flatten()
+            .associate { it }
+    }
+
+    data class Octopus(var coord: Pair<Int, Int>, var score: Int, var hasFlashed: Boolean = false) {
+        fun increment() = score++
+        private fun needsToFlash() = score > 9 && !hasFlashed
+
+        private fun flash() {
+            hasFlashed = true
+        }
+
+        fun reset() = if (hasFlashed) {
+            hasFlashed = false; score = 0; 1
+        } else 0
+
+        private fun getNeighbours(
+            validCells: OctopusGrid,
             deltas: List<Pair<Int, Int>> = listOf(
-                -1 to 0,  0 to -1,
+                -1 to 0, 0 to -1,
                 1 to 0, 0 to 1,
                 -1 to -1, 1 to 1,
                 -1 to 1, 1 to -1
-                )
+            )
         ) = deltas.asSequence()
             .map { (x2, y2) -> coord.first + x2 to coord.second + y2 }
-            .filter { validCells.contains(it) }
+            .mapNotNull { validCells[it] }
+
+        fun check(grid: OctopusGrid) {
+            if (this.needsToFlash()) {
+                this.flash()
+                this.getNeighbours(grid).forEach { it.increment(); it.check(grid) }
+            }
+        }
+    }
+
+    private fun OctopusGrid.step(): Int {
+        this.values.forEach { it.increment() }
+        this.values.forEach { oct -> oct.check(this) }
+        return this.values.sumOf { it.reset() }
+    }
+
+    object Part1 {
+        fun run() {
+            val grid = octopusGrid()
+            val count = (0..99).map { grid.step() }.sum()
+            println(count)
+        }
+    }
+
+    object Part2 {
+        private fun OctopusGrid.isNotComplete(): Boolean {
+            val flashCount = this.step()
+            return flashCount != this.size
+        }
 
         fun run() {
-            File("day11/input-test.txt")
-                .readLines(defaultCharset())
-                .mapIndexed(::toCoordinates)
-                .flatten()
-                .also { println(it) }
+            val grid = octopusGrid()
+            var count = 0
+            do {
+                count++
+            } while (grid.isNotComplete())
+
+            println(count)
         }
     }
 }
 
 fun main() {
     Day11.Part1.run()
+    Day11.Part2.run()
 }
---
+++ src/main/kotlin/Day12.kt
@@ -0,0 +1,81 @@
+import java.io.File
+import java.nio.charset.Charset.defaultCharset
+
+@OptIn(ExperimentalStdlibApi::class)
+object Day12 {
+
+    val maze = File("day12/input-real.txt")
+        .readLines(defaultCharset())
+        .flatMap { row -> row.split("-").let { listOf(it[0] to it[1], it[1] to it[0]) } }
+        .groupBy({ it.first }, { it.second })
+
+    object Part1 {
+        fun run() {
+            val paths = mutableSetOf<List<String>>()
+            countPaths(maze, paths, listOf("start"))
+            paths.forEach { println(it) }
+            println(paths.size)
+        }
+
+        private fun countPaths(
+            maze: Map<String, List<String>>,
+            paths: MutableSet<List<String>>,
+            currentPath: List<String>
+        ) {
+            val here = currentPath.last()
+            if (here == "end") {
+                paths.add(currentPath)
+            }
+            val destinations = maze[here] ?: emptyList()
+
+            destinations.forEach {
+                val nextPath = buildList { addAll(currentPath); add(it) }
+                if (!currentPath.contains(it) || it.chars().allMatch { it.toChar().isUpperCase() }) {
+                    countPaths(maze, paths, nextPath)
+                }
+            }
+        }
+    }
+
+    object Part2 {
+        fun run() {
+            val paths = mutableSetOf<List<String>>()
+            countPaths(maze, paths, listOf("start"), false)
+            println(paths.size)
+        }
+
+        private fun countPaths(
+            maze: Map<String, List<String>>,
+            paths: MutableSet<List<String>>,
+            currentPath: List<String>,
+            twiceVisited: Boolean
+        ) {
+            val here = currentPath.last()
+            if (here == "end") {
+                paths.add(currentPath)
+                return
+            }
+            val destinations = maze[here] ?: emptyList()
+
+            destinations.forEach {
+                val isNotStart = it != "start"
+                val isSmallCave = isNotStart && it.chars().allMatch { c -> c.toChar().isLowerCase() }
+                val isLargeCave = isNotStart && it.chars().allMatch { c -> c.toChar().isUpperCase() }
+
+                val nextPath = buildList { addAll(currentPath); add(it) }
+
+                when {
+                    isLargeCave -> countPaths(maze, paths, nextPath, twiceVisited)
+                    isSmallCave && !currentPath.contains(it) -> countPaths(maze, paths, nextPath, twiceVisited)
+                    isSmallCave && !twiceVisited -> countPaths(maze, paths, nextPath, true)
+                    else -> {}
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    Day12.Part1.run()
+    Day12.Part2.run()
+}
